b56f4ec6070e068a250780c8bc0da33be93cea73
maddonix <tlux14@googlemail.com>
2024-10-20 12:32:21 +0200

    basic user implementation continued (parted as sth causes commit issues)

---
+++ config/groups/main.nix
@@ -0,0 +1,20 @@
+let 
+    groups = {
+        maintenance = {
+            name = "maintenance";
+            gid = 1100;
+        };
+        service = {
+            name = "service";
+            gid = 1101;
+        };
+        logging = {
+            name = "logging";
+            gid = 1102;
+        };
+        center = {
+            name = "center";
+            gid = 1103;
+        };
+    };
+in groups
--- config/main.nix
+++ config/main.nix
@@ -2,7 +2,12 @@ let
     network-config = {
         ips = import ./ips.nix;
         hostnames = import ./hostnames.nix;
-        users = import ./users.nix;
+        users = import ./users/main.nix;
+        groups = import ./groups/main.nix;
+        hardware = import ./hardware/main;
+        paths = import ./paths/main.nix;
+        services = import ./services/main.nix;
+        scripts = import ./scripts/main.nix;
     };
 
 in network-config
