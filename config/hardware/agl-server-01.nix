c029857d9abc20939179bdbdc2a7a4f3d0a34513
maddonix <tlux14@googlemail.com>
2024-10-27 15:33:40 +0100

    add server-01 hardware

--- config/hardware/agl-server-01.nix
+++ config/hardware/agl-server-01.nix
@@ -1,3 +1,17 @@
 {
-    
+    network-interface = "enp4s0"; # select by user
+
+    secondary-network-interface = "eno1"; # select by user
+
+    nvidiaBusId = ""; # select by user
+    onboardGraphicBusId = ""; # select by user
+
+    file-system-base-uuid = "dc6ac971-d4e7-4511-9df8-245d3b00efed";
+    file-system-boot-uuid = "0778-CD65";
+    swap-device-uuid = "80978707-9b36-4bb2-ab31-9ba78f500959";
+
+    luks-hdd-intern-uuid = "";
+    luks-swap-uuid = "";
+
+    system-state = "23.11"; # enter by user
 }
