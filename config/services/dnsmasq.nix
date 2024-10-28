cf5c7bae462230e36c039a1c9a7ad9b110e33d91
Thomas <tlux14@gmail.com>
2024-10-28 15:49:35 +0100

    debugging

--- config/services/dnsmasq.nix
+++ config/services/dnsmasq.nix
@@ -17,26 +17,8 @@ let
     server-hardware = import ../hardware/${hostname}.nix;
     network-interface = server-hardware.network-interface;
 
-    extra-config = ''
-    # Listen on the VPN server IP for DNS requests
-        listen-address=127.0.0.1,${ip}
-
-        # Forward to public DNS servers
-        server=8.8.8.8
-        server=8.8.4.4
-
-        # Local domain configuration;
-        # dnsmasq will not forward queries for *.endo-reg.net to upstream DNS servers
-        local=/${domains.main}/
-
-        # Interface and logging
-        interface=${network-interface}
-        bind-interfaces # Forces dnsmasq to bind only to the interfaces specified by the listen-address and interface options
-        log-queries
-        log-facility=/var/log/dnsmasq.log
-    '';
-
     dnsmasq = {
+        enable = true;
         hostname = hostname;
         port = port;
         ip = ip;
