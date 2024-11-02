73e7f0ac1171387ff19a55954c4620ef654790ed
Thomas <tlux14@gmail.com>
2024-11-02 14:04:31 +0100

    fix path

--- config/paths/nginx.nix
+++ config/paths/nginx.nix
@@ -6,7 +6,7 @@ let
         cert-root = cert-root;
 
         ssl-certificate = "${cert-root}/__endo-reg_net_chain.pem";
-        ssl-certificate-key = "${cert-root}/endo-reg-net-lower-decrypted.key";
+        ssl-certificate-key = "${cert-root}/endo-reg_net.key";
     };
 
 in nginx
