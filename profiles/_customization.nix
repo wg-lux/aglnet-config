4b22c94f4a2f9e90866140713466142b95f0bcac
Thomas <tlux14@gmail.com>
2024-11-03 10:24:51 +0100

    asd

---
+++ profiles/_customization.nix
@@ -0,0 +1,24 @@
+{config, pkgs, ...}:
+
+let
+
+    WIREGUARD = true;
+
+    hostname = config.networking.hostName;
+    domain = config.networking.domain;
+
+    custom-packages = [] 
+        ++ (if hostname == "agl-gpu-client-02" then [ 
+            pkgs.udisks
+
+            ] else [])
+        ++ (if hostname == "agl-gpu-client-03" then []
+            else [])
+        ;
+
+in {
+    environment.systemPackages = custom-packages;
+
+
+    
+}
