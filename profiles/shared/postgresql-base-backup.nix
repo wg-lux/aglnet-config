f2bd4cc46e1d165bf6eeea2b668bab624c7c54ea
maddonix <tlux14@googlemail.com>
2024-11-10 18:05:11 +0100

    asd

--- profiles/shared/postgresql-base-backup.nix
+++ profiles/shared/postgresql-base-backup.nix
@@ -25,23 +25,24 @@ in {
         description = "Setup PostgreSQL Standby Configuration";
         wantedBy = [ "multi-user.target" ];
         before = [ "postgresql.service" ];  # Ensure this runs before the main PostgreSQL service starts
+        path = with pkgs; [ postgresql ];
         script = ''
-        if [ ! -f /var/lib/postgresql/data/standby.signal ]; then
-            # Perform base backup from primary server
-            echo "Starting base backup from primary..."
-            PGPASSFILE=/etc/replication.pgpass pg_basebackup -h 172.16.255.1 -D /var/lib/postgresql/data -U replication_user -Fp -Xs -P
-            
-            # Mark the server as a standby by creating standby.signal
-            touch /var/lib/postgresql/data/standby.signal
-            
-            # Set up replication parameters using ALTER SYSTEM
-            echo "Configuring primary_conninfo for streaming replication..."
-            psql -d postgres -c "ALTER SYSTEM SET primary_conninfo TO 'host=172.16.255.1 port=5432 user=replication_user';"
+            if [ ! -f /var/lib/postgresql/data/standby.signal ]; then
+                # Perform base backup from primary server
+                echo "Starting base backup from primary..."
+                PGPASSFILE=/etc/replication.pgpass pg_basebackup -h 172.16.255.1 -D /var/lib/postgresql/data -U replication_user -Fp -Xs -P
+                
+                # Mark the server as a standby by creating standby.signal
+                touch /var/lib/postgresql/data/standby.signal
+                
+                # Set up replication parameters using ALTER SYSTEM
+                echo "Configuring primary_conninfo for streaming replication..."
+                psql -d postgres -c "ALTER SYSTEM SET primary_conninfo TO 'host=172.16.255.1 port=5432 user=replication_user';"
 
-            # Reload PostgreSQL to apply the changes from postgresql.auto.conf
-            echo "Reloading PostgreSQL configuration..."
-            systemctl reload postgresql.service
-        fi
+                # Reload PostgreSQL to apply the changes from postgresql.auto.conf
+                echo "Reloading PostgreSQL configuration..."
+                systemctl reload postgresql.service
+            fi
         '';
     };
 
