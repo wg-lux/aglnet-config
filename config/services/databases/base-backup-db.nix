c1e594ef75ad9797018b05146ac2ff60ec34b7f0
maddonix <tlux14@googlemail.com>
2024-11-10 15:01:56 +0100

    add backup dir

--- config/services/databases/base-backup-db.nix
+++ config/services/databases/base-backup-db.nix
@@ -13,6 +13,7 @@ let
       target-db-port = conf.port;
       target-db-user = conf.replication-user;
       target-db-pass-file = data-dir + "/.pgpass";
+      backup-dir = "/backup";
 
       settings = {
             port = conf.port;
