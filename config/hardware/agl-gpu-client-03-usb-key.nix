c89669dd18593b2e9da4192cf9f98509ce0ce00f
Thomas <tlux14@gmail.com>
2024-11-28 10:10:05 +0100

    add agl gc3 keyfile

--- config/hardware/agl-gpu-client-03-usb-key.nix
+++ config/hardware/agl-gpu-client-03-usb-key.nix
@@ -1,4 +1,3 @@
-
 let 
   # Import hardware configuration based on the provided hostname config file
 
@@ -7,12 +6,34 @@ let
   # filesystem-luks-uuid = hardware.luks-hdd-intern-uuid;
   # swap-luks-uuid = hardware.luks-swap-uuid;
 
+  filesystem-luks-uuid = "4052a436-8b52-4969-adc0-f0e985fe6b6d";
+  swap-luks-uuid = "8d58f4dd-cfb1-4360-8410-60d1dad60741";
+  usb-uuid = "f3bbdd87-6e3e-441c-929e-7e3e69961041";
+  usb-mountpoint = "/mnt/usb_key";
+  usb-device = "/dev/disk/by-uuid/f3bbdd87-6e3e-441c-929e-7e3e69961041";
 
   bs = 1;
-  offset-m = 14900;
-  offset-b = 15623782400;
+  offset-m = 50;
+  offset-b = 52428800;
   keyfile-size = 4096;
 
 in {
 
+    boot.initrd.availableKernelModules = [ "dm-crypt" "sd_mod" "usb_storage"];
+
+    boot.initrd.luks.devices."luks-4052a436-8b52-4969-adc0-f0e985fe6b6d" = {
+        keyFile       = usb-device;
+        keyFileOffset = offset-b;
+        keyFileSize   = keyfile-size;
+        preLVM        = true;
+        fallbackToPassword = true;
+    };
+
+    boot.initrd.luks.devices."luks-8d58f4dd-cfb1-4360-8410-60d1dad60741" = {
+        keyFile       = usb-device;
+        keyFileOffset = offset-b;
+        keyFileSize   = keyfile-size;
+        preLVM        = true;
+        fallbackToPassword = true;
+    };
 }
